d853b8650338ebcc1b235c537e9fcb058492c4d1
Unknown <arthurcai2000@gmail.com>
2018-11-04 23:12:17 -0800

    Edits to gameplay.py

--- src/gameplay.py
+++ src/gameplay.py
@@ -1,15 +1,85 @@
 import poker
-import calc_hand
 
-def play_round(small_amt, big_amt, small, big):
-    '''Players is a list of Player instances'''
-    board = []
-    small.raise(small_amt)
-    big.raise(big_amt)
-    pot = small_amt + big_amt
-    while True:
-        s = input("What will {0} do?".format(small.name))
-        if s == 'fold' or s == 'call':
-            
+def play_game(small_amt, big_amt, start_amt):
 
-    input("What will {0} do?".format(big.name))
+    def play_rounds(players):
+        small = players[0]
+        big = players[1]
+        small.raise(small_amt)
+        big.raise(big_amt)
+        pot = small_amt + big_amt
+        most_in = big.chips_in
+        active_players = lst(players)
+
+        while len(active_players > 1):
+            while not all([p.chips_in == most_in for p in active_players]):
+                for p in active_players:
+                    print("{0}'s stack: {1}".format(p.name, str(p.stack)))
+                    print('Pot:', str(pot))
+                    if p.stack > 0:
+                        act_valid = False
+                        act = input("{0}'s action? ".format(p.name))
+                        while act_valid == False:
+                            while not in ['fold', 'check', 'call', 'raise']:
+                                act = input("Invalid action! {0}'s action? ".format(p.name))
+                            if act == 'check':
+                                if p.chips_in < most_in:
+                                    act = input("You cannot check here. {0}'s action? ".format(p.name))
+                                else:
+                                    act_valid = True
+                            elif act == 'fold':
+                                p.chips_in = 0
+                                active_players.remove(p)
+                                act_valid = True
+                            elif act == 'call':
+                                if p.stack >= (most_in - p.chips_in):
+                                    p.raise(most_in - p.chips_in)
+                                    pot += (most_in - p.chips_in)
+                                    act_valid = True
+                                else:
+                                    p.raise(p.stack)
+                                    pot += p.stack
+                                    act_valid = True
+                            elif act == 'raise':
+                                amount = input('By how much? ')
+                                if amount > p.stack or amount <= (most_in - p.chips_in):
+                                    act = input("You cannot raise that amount. {0}'s action? ".format(p.name))
+                                else:
+                                    p.raise(amount)
+                                    pot += amount
+                                    most_in = p.chips_in
+                                    act_valid = True
+
+
+            return 'First betting round complete'
+
+
+        for p in players:
+            if p.stack == 0:
+                players.remove(p)
+        if len(players) == 1:
+            return print(players[0].name, ' wins!')
+        else:
+            play_rounds(players.append(players.pop(0)))
+
+
+    player_list = []
+    num = int(input('How many players? '))
+    while num < 2:
+        print('Poker needs at least 2 players!')
+        num = int(input('How many players? '))
+    for p in range(num):
+        player_list.append(Player(input("Player {0} name? ".format(str(p + 1))), start_amt))
+    play_rounds(player_list)
+
+class Player:
+    folded = False
+    chips_in = 0
+    def __init__(self, name, stack):
+        self.name = name
+        self.stack = stack
+    def raise(self, amount):
+        self.stack -= amount
+        self.chips_in += amount
+
+play_game(1, 2, 160)
--- src/poker.py
+++ src/poker.py
@@ -14,17 +14,17 @@ class Card:
         'hearts': '♥',
         'spades': '♠'
     }
-    
+
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
-        
+
     def __lt__(self, other):
         return self.value < other.value
-    
+
     def __eq__(self, other):
         return self.value == other.value
-    
+
     def __str__(self):
         return Card.values[self.value] + Card.suits[self.suit]
 
@@ -32,26 +32,26 @@ class Hand:
     def __init__(self, size=2):
         self.reset()
         self.size = size
-    
+
     def reset(self):
         self.cards = []
-    
+
     def score(self, board):
         assert len(self.cards) == self.size
         assert len(board.cards) == board.flop + board.turn + board.river
-        
+
         all_cards = self.cards + board.cards
         total = len(all_cards)
-        
+
         max_score = 0
-        
+
         # try omitting cards i and j
         for i in range(total):
             for j in range(i+1, total):
                 max_score = max(max_score, calc_hand.score(all_cards[:i]+all_cards[i+1:j]+all_cards[j+1:]))
-        
+
         return max_score
-    
+
     def __str__(self):
         return ' '.join(str(c) for c in self.cards)
 
@@ -59,14 +59,14 @@ class Board:
     flop = 3
     turn = 1
     river = 1
-    
+
     def __init__(self):
         self.reset()
-    
+
     def reset(self):
         self.cards = []
         self.burn = []
-    
+
     def __str__(self):
         return ' '.join(str(c) for c in self.cards)
 
@@ -75,17 +75,17 @@ from random import shuffle
 class Deck:
     def __init__(self):
         self.reset()
-    
+
     def reset(self):
         self.cards = []
         for v in Card.values:
             for s in Card.suits:
                 self.cards.append(Card(v, s))
         shuffle(self.cards)
-    
+
     def pop(self):
         return self.cards.pop()
-    
+
     def deal(self, obj):
         if isinstance(obj, Hand):
             for _ in range(obj.size):
@@ -95,7 +95,8 @@ class Deck:
                 obj.burn.append(self.pop())
                 for _ in range(obj.flop): obj.cards.append(self.pop())
             elif len(obj.cards) == obj.flop:
+                obj.burn.append(self.pop())
                 for _ in range(obj.turn): obj.cards.append(self.pop())
             elif len(obj.cards) == obj.flop + obj.turn:
+                obj.burn.append(self.pop())
                 for _ in range(obj.river): obj.cards.append(self.pop())
-    
