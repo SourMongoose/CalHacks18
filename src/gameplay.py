3842201b9b309e9e349879e35bddc51ea17cde63
Unknown <arthurcai2000@gmail.com>
2019-01-11 19:52:57 -0800

    Gameplay updates

--- src/gameplay.py
+++ src/gameplay.py
@@ -8,64 +8,83 @@ def play_game(small_amt, big_amt, start_amt):
         small.bet(small_amt)
         big.bet(big_amt)
         pot = small_amt + big_amt
+        print("Small blind: {0}".format(players[0].name))
+        print("Big blind: {0}".format(players[1].name))
         most_in = big.chips_in
-        active_players = list(players)
-        order_play = active_players[2:] + active_players[:2]
+        last = 1
+        current = 2
 
-        while len(active_players) > 1:
 
-            while not all([p.chips_in == most_in for p in active_players]):
-                for p in list(order_play):
-                    print("{0}'s stack: {1}".format(p.name, str(p.stack)))
+        for _ in range(4):
+            while True:
+                cp = players[current]
+                if cp.still_in == False:
+                    current = (current + 1) % len(players)
+                else:
+                    print("{0}'s stack: {1}".format(cp.name, str(cp.stack)))
                     print('Pot:', str(pot))
-                    if p.stack > 0:
+                    if cp.stack > 0:
                         act_valid = False
-                        act = input("{0}'s action? ".format(p.name))
+                        act = input("{0}'s action? ".format(cp.name))
                         while act_valid == False:
                             if act.lower() not in ['fold', 'check', 'call', 'raise']:
-                                act = input("Invalid action! {0}'s action? ".format(p.name))
+                                act = input("Invalid action! {0}'s action? ".format(cp.name))
                             if act.lower() == 'check':
-                                if p.chips_in < most_in:
-                                    act = input("You cannot check here. {0}'s action? ".format(p.name))
+                                if cp.chips_in < most_in:
+                                    act = input("You cannot check here. {0}'s action? ".format(cp.name))
                                 else:
                                     act_valid = True
                             elif act.lower() == 'fold':
-                                p.chips_in = 0
-                                order_play.remove(p)
-                                active_players.remove(p)
+                                cp.chips_in = 0
+                                cp.still_in = False
                                 act_valid = True
                             elif act.lower() == 'call':
-                                if p.stack >= (most_in - p.chips_in):
-                                    pot += (most_in - p.chips_in)
-                                    p.bet(most_in - p.chips_in)
+                                if cp.stack >= (most_in - cp.chips_in):
+                                    pot += (most_in - cp.chips_in)
+                                    cp.bet(most_in - cp.chips_in)
                                     act_valid = True
                                 else:
-                                    p.bet(p.stack)
-                                    pot += p.stack
+                                    cp.bet(cp.stack)
+                                    pot += cp.stack
                                     act_valid = True
                             elif act.lower() == 'raise':
                                 amount = int(input('By how much? '))
-                                if amount > p.stack or amount <= (most_in - p.chips_in):
-                                    act = input("You cannot bet that amount. {0}'s action? ".format(p.name))
+                                if amount > cp.stack or amount <= (most_in - cp.chips_in):
+                                    act = input("You cannot bet that amount. {0}'s action? ".format(cp.name))
                                 else:
-                                    p.bet(amount)
+                                    cp.bet(amount)
                                     pot += amount
-                                    most_in = p.chips_in
-                                    order_play = order_play[:order_play.index(p)]
+                                    most_in = cp.chips_in
+                                    last = (current + len(players) - 1) % len(players)
                                     act_valid = True
+                    if current == last:
+                        break
+                    current = (current + 1) % len(players)
+            print('bets done')
+            if len([p in players if p.still_in]) == 1:
+                for p in players:
+                    if p.still_in:
+                        print("{0} wins {1}!".format(p.name, pot))
+                `       p.stack += pot
+                        p.chips_in = 0
+                break
+            current = 0
+            last = len(players) - 1
 
+        #cards_shown_case
 
-            print('first bets done')
-            return
 
 
         for p in list(players):
+            p.still_in = True
+            p.chips_in = 0
             if p.stack == 0:
                 players.remove(p)
         if len(players) == 1:
             return print(players[0].name, ' wins!')
         else:
-            play_rounds(players.append(players.pop(0)))
+            players.append(players.pop(0))
+            play_rounds(players)
 
 
     player_list = []
@@ -81,7 +100,7 @@ def play_game(small_amt, big_amt, start_amt):
     play_rounds(player_list)
 
 class Player:
-    folded = False
+    still_in = True
     chips_in = 0
     def __init__(self, name, stack):
         self.name = name
